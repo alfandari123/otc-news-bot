5593430ec48d04dbdbdf277935e339e99e9299e9
alfandari123 <alfandari1991@gmail.com>
2026-06-15 20:06:41 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,9 +1,27 @@
 import json
+import feedparser
 
 with open("watchlist.json", "r") as f:
     watchlist = json.load(f)
 
-print("=== WATCHLIST ===")
+with open("seen_news.json", "r") as f:
+    seen_news = json.load(f)
 
 for symbol in watchlist:
-    print(f"Checking {symbol}")
+
+    url = f"https://feeds.finance.yahoo.com/rss/2.0/headline?s={symbol}&region=US&lang=en-US"
+
+    feed = feedparser.parse(url)
+
+    if not feed.entries:
+        print(f"No news found for {symbol}")
+        continue
+
+    latest = feed.entries[0]
+
+    title = latest.title
+    link = latest.link
+
+    print(f"Symbol: {symbol}")
+    print(f"Title: {title}")
+    print(f"Link: {link}")
