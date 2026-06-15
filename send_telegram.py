7758ee685ffb94467a579f144c86fb712dcb1766
alfandari123 <alfandari1991@gmail.com>
2026-06-15 19:29:20 +0300

    Create send_telegram.py

---
+++ send_telegram.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+message = "🚀 OTC Scanner Online"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Telegram message sent")
