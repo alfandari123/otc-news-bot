a27a6002c4225a86550d868be3ae07c3b21ea083
alfandari123 <alfandari1991@gmail.com>
2026-06-15 20:29:42 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,27 +1,26 @@
 import json
-import feedparser
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 with open("watchlist.json", "r") as f:
     watchlist = json.load(f)
 
-with open("seen_news.json", "r") as f:
-    seen_news = json.load(f)
+message = "🔎 OTC Scanner Report\n\n"
 
 for symbol in watchlist:
+    message += f"Checking: {symbol}\n"
 
-    url = f"https://feeds.finance.yahoo.com/rss/2.0/headline?s={symbol}&region=US&lang=en-US"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    feed = feedparser.parse(url)
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
 
-    if not feed.entries:
-        print(f"No news found for {symbol}")
-        continue
-
-    latest = feed.entries[0]
-
-    title = latest.title
-    link = latest.link
-
-    print(f"Symbol: {symbol}")
-    print(f"Title: {title}")
-    print(f"Link: {link}")
+print("Report sent")
