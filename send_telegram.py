49666672e4aaeaffcd2bc1fc80d7d50c8bad5ba8
alfandari123 <alfandari1991@gmail.com>
2026-06-17 15:13:48 +0300

    Update send_telegram.py

--- send_telegram.py
+++ send_telegram.py
@@ -1,17 +1,11 @@
-import json
 import os
 import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-with open("watchlist.json", "r") as f:
-    watchlist = json.load(f)
-
-message = "📈 OTC Watchlist\n\n"
-
-for symbol in watchlist:
-    message += f"• {symbol}\n"
+with open("telegram_message.txt", "r") as f:
+    message = f.read()
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
