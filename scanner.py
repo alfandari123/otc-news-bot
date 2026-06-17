274cfca1b3bfeaf02aa02441ee6b942c8a22cf82
alfandari123 <alfandari1991@gmail.com>
2026-06-17 15:05:10 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,10 +1,13 @@
 import json
 
-def scan_otc():
-    return ["AITX", "SONN", "GVSI"]
+WATCHLIST = [
+    "AITX",
+    "SONN",
+    "GVSI",
+    "HMBL",
+    "ILUS",
+    "ABQQ"
+]
 
-if __name__ == "__main__":
-    data = scan_otc()
-
-    with open("watchlist.json", "w") as f:
-        json.dump(data, f)
+with open("watchlist.json", "w") as f:
+    json.dump(WATCHLIST, f)
