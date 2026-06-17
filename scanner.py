bc7ffa1470c85e7ca32797645d37bbabe064921e
alfandari123 <alfandari1991@gmail.com>
2026-06-17 15:12:34 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,13 +1,38 @@
+import yfinance as yf
+import feedparser
 import json
 
-WATCHLIST = [
-    "AITX",
-    "SONN",
-    "GVSI",
-    "HMBL",
-    "ILUS",
-    "ABQQ"
-]
+with open("watchlist.json", "r") as f:
+    watchlist = json.load(f)
 
-with open("watchlist.json", "w") as f:
-    json.dump(WATCHLIST, f)
+message = "📈 OTC Scanner Update\n\n"
+
+for symbol in watchlist:
+    try:
+        stock = yf.Ticker(symbol)
+        data = stock.history(period="1d")
+
+        if not data.empty:
+            price = data["Close"].iloc[-1]
+            message += f"🔹 {symbol}\n"
+            message += f"💵 Price: {price:.6f}\n\n"
+        else:
+            message += f"🔹 {symbol}\nNo data\n\n"
+
+    except Exception as e:
+        message += f"🔹 {symbol}\nError\n\n"
+
+
+message += "📰 Latest OTC News\n\n"
+
+news = feedparser.parse(
+    "https://news.google.com/rss/search?q=OTC+stock"
+)
+
+for item in news.entries[:5]:
+    message += f"• {item.title}\n"
+
+with open("telegram_message.txt", "w") as f:
+    f.write(message)
+
+print("Scanner finished")
