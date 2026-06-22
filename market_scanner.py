c34847e9118929cd6eee8973a13897ea535feba0
alfandari123 <alfandari1991@gmail.com>
2026-06-22 20:43:27 +0300

    Update market_scanner.py

--- market_scanner.py
+++ market_scanner.py
@@ -2,6 +2,29 @@ import requests
 import os
 from datetime import datetime
 
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+
+def get_stock_data(symbol):
+
+    url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
+
+    try:
+        response = requests.get(url).json()
+
+        result = response["chart"]["result"][0]
+
+        price = result["meta"]["regularMarketPrice"]
+
+        return price
+
+    except:
+        return None
+
+
+
 def scan_market():
 
     stocks = [
@@ -10,22 +33,34 @@ def scan_market():
         "GVSI"
     ]
 
-    message = "📊 OTC Scanner Report\n\n"
+    message = "📊 OTC Scanner Live\n\n"
 
     for stock in stocks:
-        message += f"• {stock} - scanned ✅\n"
 
-    message += f"\n🕒 {datetime.now()}"
+        price = get_stock_data(stock)
+
+        if price:
+
+            message += (
+                f"🚨 {stock}\n"
+                f"💵 Price: ${price}\n\n"
+            )
+
+        else:
+            message += f"⚠️ {stock} - no data\n\n"
+
+
+    message += f"🕒 {datetime.now()}"
 
     return message
 
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
 
 message = scan_market()
 
+
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
 
 requests.post(
     url,
@@ -35,4 +70,5 @@ requests.post(
     }
 )
 
-print("Scanner finished")
+
+print("Live scanner finished")
