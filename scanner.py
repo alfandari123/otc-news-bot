7703454e2d52ab9deb781f9047081182773c132f
alfandari123 <alfandari1991@gmail.com>
2026-06-22 21:34:05 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,30 +1,12 @@
 import requests
 import os
-from datetime import datetime
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+print("TOKEN:", BOT_TOKEN[:10] if BOT_TOKEN else "MISSING")
+print("CHAT:", CHAT_ID)
 
-def scan_market():
-
-    stocks = [
-        "AITX",
-        "GVSI",
-        "SONN"
-    ]
-
-    message = "📊 OTC Scanner Report\n\n"
-
-    for stock in stocks:
-        message += f"• {stock} - scanned ✅\n"
-
-    message += f"\n🕒 {datetime.now()}"
-
-    return message
-
-
-message = scan_market()
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
@@ -32,7 +14,7 @@ response = requests.post(
     url,
     data={
         "chat_id": CHAT_ID,
-        "text": message
+        "text": "🚀 Test from scanner.py"
     }
 )
 
