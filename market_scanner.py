6cb5471b6dfc92f549f563068a934fcaa1e0b0c0
alfandari123 <alfandari1991@gmail.com>
2026-06-22 21:46:20 +0300

    Update market_scanner.py

--- market_scanner.py
+++ market_scanner.py
@@ -1,33 +1,28 @@
-import requests
 import os
+import yfinance as yf
+import requests
 from datetime import datetime
+
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 
-def get_price(symbol):
-
-    url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}"
-
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+def get_stock(symbol):
 
     try:
-        r = requests.get(url, headers=headers, timeout=10)
+        stock = yf.Ticker(symbol)
 
-        data = r.json()
-
-        price = data["chart"]["result"][0]["meta"]["regularMarketPrice"]
+        price = stock.fast_info["last_price"]
 
         return price
 
-    except Exception as e:
-        return "No data"
+    except Exception:
+        return None
 
 
-def scan():
+
+def scan_market():
 
     stocks = [
         "AITX",
@@ -35,21 +30,29 @@ def scan():
         "SONN"
     ]
 
-    msg = "📊 OTC Scanner Live\n\n"
+    message = "📈 OTC Scanner Live\n\n"
 
-    for s in stocks:
-        price = get_price(s)
-        msg += f"• {s}: {price}\n"
+    for stock in stocks:
 
-    msg += f"\n🕒 {datetime.now()}"
+        price = get_stock(stock)
 
-    return msg
+        if price:
+            message += f"🚨 {stock}\n💵 Price: {price}\n\n"
+        else:
+            message += f"⚠️ {stock} no data\n\n"
 
 
-message = scan()
+    message += f"🕒 {datetime.now()}"
+
+    return message
+
+
+
+message = scan_market()
 
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
 
 requests.post(
     url,
@@ -59,4 +62,5 @@ requests.post(
     }
 )
 
-print("Sent to Telegram")
+
+print("Scanner sent")
